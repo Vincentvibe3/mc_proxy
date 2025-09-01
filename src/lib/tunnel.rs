ec4e72252a635fffadc1aba047ce551874a8e524
Vincent <vincentvibe3@gmail.com>
2025-09-01 16:54:47 -0400

    change copy method

--- src/lib/tunnel.rs
+++ src/lib/tunnel.rs
@@ -2,20 +2,22 @@ use std::error::Error;
 
 use bytes::{Bytes, BytesMut};
 use quinn::{RecvStream, SendStream};
-use tokio::{io::{self, copy_bidirectional, AsyncWriteExt}, net::tcp::{OwnedReadHalf, OwnedWriteHalf}, sync::mpsc::{self, Receiver}};
+use tokio::{io::{self, copy, copy_bidirectional, AsyncWriteExt}, net::tcp::{OwnedReadHalf, OwnedWriteHalf}, sync::mpsc::{self, Receiver}};
 
 pub async fn quic_to_tcp(mut read:RecvStream, mut send:OwnedWriteHalf)-> Result<(), Box<dyn Error>>{
-    let (channel_send, channel_recv): (mpsc::Sender<Bytes>, Receiver<Bytes>) = mpsc::channel(500);
-    tokio::spawn(async move {
-        tcp_send(send, channel_recv).await;
-    });
-    loop {
-		// println!("q read");
-        let chunk_opt = read.read_chunk(4096, true).await.unwrap();
-        if let Some(data) = chunk_opt {
-            channel_send.send(data.bytes).await?;
-        }
-    }
+	copy(&mut read, &mut send).await?;
+    // let (channel_send, channel_recv): (mpsc::Sender<Bytes>, Receiver<Bytes>) = mpsc::channel(500);
+    // tokio::spawn(async move {
+    //     tcp_send(send, channel_recv).await;
+    // });
+    // loop {
+	// 	// println!("q read");
+    //     let chunk_opt = read.read_chunk(4096, true).await.unwrap();
+    //     if let Some(data) = chunk_opt {
+    //         channel_send.send(data.bytes).await?;
+    //     }
+    // }
+	Ok(())
 }
 
 async fn tcp_send(mut send:OwnedWriteHalf, mut channel:Receiver<Bytes>)-> Result<(), Box<dyn Error>>{
@@ -45,33 +47,34 @@ async fn quic_send(mut send:SendStream, mut channel:Receiver<Bytes>)-> Result<()
     Ok(())
 }
 
-pub async fn tcp_to_quic(read:OwnedReadHalf, send:SendStream, mut data:BytesMut) -> Result<(), Box<dyn Error>>{
+pub async fn tcp_to_quic(mut read:OwnedReadHalf, mut send:SendStream, mut data:BytesMut) -> Result<(), Box<dyn Error>>{
     let (channel_send, channel_recv): (mpsc::Sender<Bytes>, Receiver<Bytes>) = mpsc::channel(500);
-    tokio::spawn(async move {
-        quic_send(send, channel_recv).await;
-    });
-    loop {
-		// println!("t read");
-        read.readable().await?;
-        if data.len() == data.capacity(){
-            data.reserve(data.len()+1024);
-        }
-        match read.try_read_buf(&mut data) {
-            Ok(0) => break,
-            Ok(n) => {
-                // println!("read {} bytes", n);
-            }
-            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
-                // continue;
-            }
-            Err(e) => {
-                return Err(e.into());
-            }
-        }
-		if data.len() != 0 {
-            channel_send.send(data.split().freeze()).await?;
-        }
+    // tokio::spawn(async move {
+    //     quic_send(send, channel_recv).await;
+    // });
+	copy(&mut read, &mut send).await?;
+    // loop {
+	// 	// println!("t read");
+    //     read.readable().await?;
+    //     if data.len() == data.capacity(){
+    //         data.reserve(data.len()+1024);
+    //     }
+    //     match read.try_read_buf(&mut data) {
+    //         Ok(0) => break,
+    //         Ok(n) => {
+    //             // println!("read {} bytes", n);
+    //         }
+    //         Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+    //             // continue;
+    //         }
+    //         Err(e) => {
+    //             return Err(e.into());
+    //         }
+    //     }
+	// 	if data.len() != 0 {
+    //         channel_send.send(data.split().freeze()).await?;
+    //     }
         
-    }
+    // }
     Ok(())
 }
